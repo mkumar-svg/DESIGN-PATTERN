00aa746ce637872b67266db1ff711af009fa9144
mkumar-svg <mkumar800534@gmail.com>
2024-10-11 16:44:45 +0530

    Design Pattern

---
+++ Design_Pattern/src/com/nit/singleton/design/pattern/EagerInitializedSingleton.java
@@ -0,0 +1,16 @@
+package com.nit.singleton.design.pattern;
+
+public class EagerInitializedSingleton {
+	
+	private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+	
+	// private constructor to avoid client applications using the constructor
+	private EagerInitializedSingleton() {
+		
+	}
+	
+	public static EagerInitializedSingleton getInstance() {
+		return instance;
+	}
+
+}
